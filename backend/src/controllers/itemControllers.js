017f0635c4737a7d5024dd35f374b9e03c2ebac3
Simon Bullado <102648783+simbld@users.noreply.github.com>
2023-01-28 17:28:31 +0100

    Initialize back-without-db

---
+++ backend/src/controllers/itemControllers.js
@@ -0,0 +1,116 @@
+const models = require("../models");
+
+/*
+
+// without db
+
+const items = [
+  { id: 1, title: "Stuff" },
+  { id: 2, title: "Doodads" },
+];
+
+const browse = (req, res) => {
+  res.send(items);
+};
+
+const read = (req, res) => {
+  const existingItem = items.find(
+    (item) => item.id === parseInt(req.params.id, 10)
+  );
+
+  if (existingItem == null) {
+    res.sendStatus(404);
+  } else {
+    res.send(existingItem);
+  }
+};
+
+*/
+
+// with db
+
+const browse = (req, res) => {
+  models.item
+    .findAll()
+    .then(([rows]) => {
+      res.send(rows);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const read = (req, res) => {
+  models.item
+    .find(req.params.id)
+    .then(([rows]) => {
+      if (rows[0] == null) {
+        res.sendStatus(404);
+      } else {
+        res.send(rows[0]);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const edit = (req, res) => {
+  const item = req.body;
+
+  item.id = parseInt(req.params.id, 10);
+
+  models.item
+    .update(item)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const add = (req, res) => {
+  const item = req.body;
+
+  models.item
+    .insert(item)
+    .then(([result]) => {
+      res.location(`/items/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const destroy = (req, res) => {
+  models.item
+    .delete(req.params.id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  browse,
+  read,
+  edit,
+  add,
+  destroy,
+};
